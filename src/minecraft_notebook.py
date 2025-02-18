7f97deb8b0713c973f5c35067f7f79cf242e41bf
chemitaro <iwasi_44@hotmail.com>
2025-02-19 00:19:46 +0900

    Enhance agent positioning and teleportation with player-aware turning

--- src/minecraft_notebook.py
+++ src/minecraft_notebook.py
@@ -1,16 +1,52 @@
-agent.say(player.position)
-
-
-def agent_teleport_player():
-    agent.teleport(["^0", "^0", "^1"])
-
 def set_agent_azimuth(azimuth: int):
+    """
+    エージェントの向く方角を変える
+    """
     for i in range(4):
         if agent.rotation == azimuth:
             break
         agent.turn("right")
 
-set_agent_azimuth(90)
+def agent_turn_away_from_player() -> None:
+    """
+    エージェントがプレイヤーに背を向ける動作を実行します。
+    """
+    agent_position = agent.position
+    player_positoon = player.position
+    agent.say(agent_position)
+    agent.say(player_positoon)
+    azimuth = None
+
+    # x軸かz軸でどちらが近いか判断する
+    if abs(abs(agent_position.x) - abs(player_positoon.x)) > abs(abs(agent_position.z) - abs(player_positoon.z)):
+        # x軸の差が大きい場合
+        if agent_position.x < player_positoon.x:
+            # 西向き
+            azimuth = 90
+        else:
+            # 東向き
+            azimuth = -90
+    else:
+        # z軸の差が大きい場合
+        if agent_position.z < player_positoon.z:
+            # 北向き
+            azimuth = -180
+        else:
+            # 南向き
+            azimuth = 0
+    agent.say(azimuth)
+    
+    set_agent_azimuth(azimuth)
+
+
+
+def agent_teleport_player():
+    """
+    エージェントがプレイヤーの前にテレポートする
+    """
+    agent.teleport(["^0", "^0", "^1"])
+    agent_turn_away_from_player()
+
 
 
 
